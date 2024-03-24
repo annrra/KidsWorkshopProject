abc655722565cdfbeb0fa7b6d512df4ff435e624
annrra <annrra@gmail.com>
2024-03-24 11:38:59 +0200

    Opt out caching for post by slug fetch

--- lib/api.ts
+++ lib/api.ts
@@ -328,7 +328,7 @@ export async function getPostBySlug(slug: string) {
   const res = await fetch(API_URL, {
     method: "POST",
     headers: {'Content-Type': 'application/json'},
-    next: { tags: ['collection'] },
+    cache: 'no-store',
     body: JSON.stringify({
       query:`{
         post(id: "${slug}", idType: URI) {
