46f0f4518d1bb9933549d902268d41727b6cf6b9
annrra <annrra@gmail.com>
2026-02-18 12:29:38 +0200

    Set up Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({
         <ThemeContextProvider>
           <div className={styles.mode}>
             {children}
+            <Analytics />
           </div>
         </ThemeContextProvider>
       </body>
